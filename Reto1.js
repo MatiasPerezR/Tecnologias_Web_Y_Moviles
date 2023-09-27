03ab376a2f7dbdc561cb101215da24dcfe26fc57
MatiasPerezR <perezrosasmatias@gmail.com>
2023-09-27 14:39:55 -0300

    Retos

---
+++ Reto1.js
@@ -0,0 +1,8 @@
+
+let numero = prompt("Ingrese un numero:")
+
+  if (numero % 2 == 0 ) {
+    console.log(numero + " es un numero par.")
+  } else {
+    console.log(numero + " es un numero impar.")
+  }
