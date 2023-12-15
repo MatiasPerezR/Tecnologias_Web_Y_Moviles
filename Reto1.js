381820d9104e4edcac9efc32e9a825c54d5f8350
M <PerezRosasMatias@gmail.com>
2023-12-15 06:18:51 -0300

    Delete Reto1.js

--- Reto1.js
+++
@@ -1,8 +0,0 @@
-
-let numero = prompt("Ingrese un numero:")
-
-  if (numero % 2 == 0 ) {
-    console.log(numero + " es un numero par.")
-  } else {
-    console.log(numero + " es un numero impar.")
-  }
